12ded20bb0a4212047ddf5b38f2eb04c4550ed4f
Rafael Lino <rafael.veloso.lino@hotmail.com>
2020-03-31 10:59:50 -0300

    [UPD] odoo.conf e mudar o nome da pasta do projeto meus modulos

---
+++ custom/meu_modulo/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+        'name': 'meu modulo test',
+        'description': 'Descrição do meu módulo',
+        'depends': [
+                'base',
+        ],
+        'author': 'EU mesmo',
+        'website': 'http://site.com',
+        'version': '1.0',
+        'category': 'Test',
+        'summary': 'Test para modulo',
+}
