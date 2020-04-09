e45f4311cf59f8c2b1a61d91c2b0163b15927d42
Rafael Lino <rafael.veloso.lino@hotmail.com>
2020-04-09 08:30:03 -0300

    Add build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
 sudo apt install gcc build-essential python3-dev libxslt1-dev libldap2-dev libsasl2-dev \libzip-dev libssl-dev libxml2-dev
+pip install bobtemplates.odoo
+alias bob-addon='mrbob bobtemplates.odoo:addon'
+alias bob-model='mrbob bobtemplates.odoo:model'
+alias bob-test='mrbob bobtemplates.odoo:test'
+alias bob-wizard='mrbob bobtemplates.odoo:wizard'
